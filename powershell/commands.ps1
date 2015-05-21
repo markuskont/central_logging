33a8deca52e565d03604c11337447e3bed0f7438
Markus Kont <markuskont@gmail.com>
2015-05-21 16:53:34 +0300

    File download thorugh basic authentication. UNTESTED.

--- powershell/commands.ps1
+++ powershell/commands.ps1
@@ -4,7 +4,9 @@ Invoke-WebRequest "http://downloads.sourceforge.net/project/nxlog-ce/nxlog-ce-2.
 # Install nxlog .msi file
 Start-Process ".\nxlog.msi" /qn -Wait
 
-cd 'C:\Program Files (x86)\nxlog\conf'
+$confdir='C:\Program Files (x86)\nxlog\conf'
+
+cd $confdir
 
 # Download NXLog configuration file
 Invoke-WebRequest "https://raw.githubusercontent.com/markuskont/central_logging/master/NXLog/nxlog.conf" -OutFile .\nxlog.tpl
@@ -20,3 +22,34 @@ $server="CHANGEME"
 $port="514"
 
 Get-Content .\nxlog.tpl | ForEach-Object {$_ -Replace "__CAFILE__","$cafile" ` -Replace "__CERTFILE__","$certfile" ` -Replace "__KEYFILE__","$keyfile" ` -Replace "__SERVER__","$server" ` -Replace "__PORT__","$port"} | Out-File .\nxlog.conf
+
+# Maybe this will work
+
+$certdir = 'C:\Program Files (x86)\nxlog\cert'
+
+$uri="CHANGEME"
+
+$user='CHANGEME'
+$pwd='CHANGEME'
+
+$wc = new-object System.Net.WebClient
+$credCache = new-object System.Net.CredentialCache
+$creds = new-object System.Net.NetworkCredential($user,$pwd)
+
+$source = ($uri + '/' + $keyfile)
+$destination =($certdir + '\' + $keyfile)
+$credCache.Add($source, "Basic", $creds)
+$wc.Credentials = $credCache
+$wc.DownloadFile($source, $destination)
+
+$source = ($uri + '/' + $certfile)
+$destination =($certdir + '\' + $certfile)
+$credCache.Add($source, "Basic", $creds)
+$wc.Credentials = $credCache
+$wc.DownloadFile($source, $destination)
+
+$source = ($uri + '/' + $ccfile)
+$destination =($certdir + '\' + $cafile)
+$credCache.Add($source, "Basic", $creds)
+$wc.Credentials = $credCache
+$wc.DownloadFile($source, $destination)
